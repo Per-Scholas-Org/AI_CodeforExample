ff5cf7702e36fed52d2f5ba6705550dbb26209c8
Haseeb <haseeb.dev1988@gmail.com>
2024-03-12 22:01:31 -0400

    Add files via upload

---
+++ EXampleFour.java
@@ -0,0 +1,13 @@
+public class EXampleFour {
+    // Java  program for simple interest , Takes principal, rate and time as input from user
+//Calculates and prints simple interest.
+
+    public static void main(String[] args) {
+        double principal = 1000.0;
+        double rate = 10.0;
+        double time = 2.0;
+        double simpleInterest = (principal * rate * time) / 100;
+        System.out.println("Simple Interest: " + simpleInterest);
+
+    }
+}
